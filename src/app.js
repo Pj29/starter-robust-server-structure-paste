d16f0839edac0a55dd3d6fdf6ca49c82a7c3770d
Peter Jones <pjxcvi@proton.me>
2023-09-11 19:06:14 -0400

    finish error handler

--- src/app.js
+++ src/app.js
@@ -4,41 +4,53 @@ const app = express();
 // TODO: Follow instructions in the checkpoint to implement ths API.
 
 const pastes = require("./data/pastes-data");
-app.use(express.json()); 
+app.use(express.json());
 
-app.use("/pastes/:pasteId", (req, res) => {
+app.use("/pastes/:pasteId", (req, res, next) => {
   const { pasteId } = req.params;
   const foundPaste = pastes.find((paste) => paste.id === Number(pasteId));
 
   if (foundPaste) {
     res.json({ data: foundPaste });
   } else {
-    next(`Paste id not found: ${pasteId}`);
+    next({ status: 404, message: `Paste id not found: ${pasteId}` });
   }
 });
 
-// Variable to hold the next ID
-// Because some IDs may already be used, find the largest assigned ID
+// New middleware function to validate the request body
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next(); // Call `next()` without an error message if the result exists
+  }
+  next({
+    status: 400,
+    message: "A 'text' property is required.",
+  });
+}
+
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
-app.post("/pastes", (req, res, next) => {
-  const { data: { name, syntax, exposure, expiration, text, user_id } = {} } = req.body;
-  if (text) {
-  const newPaste = {
-    id: ++lastPasteId, // Increment last ID, then assign as the current ID
-    name,
-    syntax,
-    exposure,
-    expiration,
-    text,
-    user_id,
-  };
-  pastes.push(newPaste);
-  res.status(201).json({ data: newPaste });
-} else {
-  res.sendStatus(400);
+app.post(
+  "/pastes",
+  bodyHasTextProperty, // Add validation middleware function
+  (req, res) => {
+    // Route handler no longer has validation code.
+    const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
+      req.body;
+    const newPaste = {
+      id: ++lastPasteId, // Increment last id then assign as the current ID
+      name,
+      syntax,
+      exposure,
+      expiration,
+      text,
+      user_id,
+    };
+    pastes.push(newPaste);
+    res.status(201).json({ data: newPaste });
   }
-});
+);
 
 app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
@@ -46,13 +58,14 @@ app.get("/pastes", (req, res) => {
 
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+  next({ status: 404, message: `Paste id not found: ${pasteId}` });
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
